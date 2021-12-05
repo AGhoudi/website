661701ad9cb974aeb6798a8cd29a0a9dcc11d335
Link1986 <ali.ghoudi@gmail.com>
2021-12-05 23:42:24 +0100

    updated discover.tsx and discover.scss

--- src/components/discover/discover.tsx
+++ src/components/discover/discover.tsx
@@ -7,9 +7,9 @@ const Discover: React.FunctionComponent = () => (
   <section id="discover">
     <BlueRibbon title="Me Découvrir" />
     <div className="content">
-      <div className="content-box">
-        <div className="text-box">
-          <p className="presentation">
+      <div className="content__box">
+        <div className="content__text">
+          <p className="content__presentation">
           Je m'appelle <span>Ali Ghoudi</span>, je suis intégrateur web et développeur d'applications.<br />
           Dans le domaine informatique depuis plusieurs années,
           je me suis spécialisé<br /> au fur et à mesure dans les langages Javascript et PHP.<br /> 
